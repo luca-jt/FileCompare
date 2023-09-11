8172d799e724d50c9753c81adcb5a8812d80ae79
luca-jt <random.lulu@die-genossen.de>
2023-09-12 00:16:36 +0200

    restructure with 'with'-statements and cleanup

--- compare_files.py
+++ compare_files.py
@@ -1,15 +1,17 @@
 from sys import argv
 from os.path import isfile
 
-inputFile1 = argv[1]
-inputFile2 = argv[2]
+# change the inputFile.. variables for actual use:
+# inputFile1 = argv[1]
+# inputFile2 = argv[2]
+inputFile1 = "testfile1.txt"
+inputFile2 = "testfile2.txt"
+
 outputFile = "diff.patch"
 
 if not isfile(inputFile1) or not isfile(inputFile2):
     raise ValueError("Invalid file paths.")
 
-file1 = (f1 := open(inputFile1)).readlines()
-file2 = (f2 := open(inputFile2)).readlines()
 diff = ["diff\n", "--- " + inputFile1 + " | +++" + inputFile2 + "\n"]
 
 
@@ -25,48 +27,50 @@ def index_if_line_contained(lines: list[str], line: str, index=0) -> int:
 
 
 def parse_changes(d: list[str]):
-    out = open(outputFile, 'w')
-
-    for i in range(len(d)):
-        out.write(d[i])
-    out.close()
+    with open(outputFile, 'w') as out:
+        for i in d:
+            out.write(i)
 
 
 def main():
 
-    i = j = 0
-    while i < len(file1) or j < len(file2):
+    with open(inputFile1) as f1:
+        file1 = f1.readlines()
 
-        line1 = file1[i] if i < len(file1) else ""
-        line2 = file2[j] if j < len(file2) else ""
+        with open(inputFile2) as f2:
+            file2 = f2.readlines()
 
-        if line1 != line2:
+            i = j = 0
+            while i < len(file1) or j < len(file2):
 
-            if (found_index := index_if_line_contained(file2, line1, j)) > 0:
-                for k in range(j, found_index):
-                    add_to_diff(file2[k], sign="+")
-                    j = found_index
+                line1 = file1[i] if i < len(file1) else ""
+                line2 = file2[j] if j < len(file2) else ""
 
-            elif (found_index := index_if_line_contained(file1, line2, i)) > 0:
-                for k in range(i, found_index):
-                    add_to_diff(file1[k], sign="-")
-                    i = found_index
+                if line1 != line2:
 
-            else:
-                add_to_diff(line2, sign="+")
-                j += 1
-                add_to_diff(line1, sign="-")
-                i += 1
+                    if (found_index := index_if_line_contained(file2, line1, j)) > 0:
+                        for k in range(j, found_index):
+                            add_to_diff(file2[k], sign="+")
+                            j = found_index
 
-        else:
-            add_to_diff(line1)
-            i += 1
-            j += 1
+                    elif (found_index := index_if_line_contained(file1, line2, i)) > 0:
+                        for k in range(i, found_index):
+                            add_to_diff(file1[k], sign="-")
+                            i = found_index
 
-    parse_changes(diff)
+                    else:
+                        add_to_diff(line2, sign="+")
+                        j += 1
+                        add_to_diff(line1, sign="-")
+                        i += 1
+
+                else:
+                    add_to_diff(line1)
+                    i += 1
+                    j += 1
+
+            parse_changes(diff)
 
 
 if __name__ == "__main__":
     main()
-    f1.close()
-    f2.close()
